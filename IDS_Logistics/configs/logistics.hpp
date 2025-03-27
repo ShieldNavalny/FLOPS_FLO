169c1d34e87dc621bd5b34dc8f6aa3987f28dfc2
Azraeel <kanyepayton1@gmail.com>
2025-03-27 23:55:20 +0900

    Add Storage to IDS

--- IDS_Logistics/configs/logistics.hpp
+++ IDS_Logistics/configs/logistics.hpp
@@ -15,3 +15,27 @@ class StorageBladder_02_water_sand_F {
     category = "Logistics";
     cost = 15;
 };
+class Land_PlasticCase_01_large_olive_F {
+    category = "Logistics";
+    cost = 15;
+};
+class Land_PlasticCase_01_medium_olive_F {
+    category = "Logistics";
+    cost = 15;
+};
+class Land_PlasticCase_01_small_olive_F {
+    category = "Logistics";
+    cost = 15;
+};
+class Land_MetalCase_01_medium_F {
+    category = "Logistics";
+    cost = 15;
+};
+class Land_MetalCase_01_large_F {
+    category = "Logistics";
+    cost = 15;
+};
+class Land_MetalCase_01_small_F {
+    category = "Logistics";
+    cost = 15;
+};
